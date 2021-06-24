c283f7606ef6994c1384a0263b055407da55e722
Larskerstens <larskerstens10@gmail.com>
2021-06-24 23:40:58 +0200

    seo done

--- next-seo.config.js
+++ next-seo.config.js
@@ -12,9 +12,9 @@ export default {
     description:
       "Welkom op ons nieuw korfbalplatform waar je alles vind over de club, wat je moet weten van wedstrijden tot trainingen tot leden",
     images: {
-      url: "../public/images/clubs/korfbal.png",
-      width: 250,
-      height: 200,
+      url: "https://s3.eu-central-1.amazonaws.com/twizzit-image-cache/media_xxlarge/media/697/5b375afdb2261.JPG",
+      width: 500,
+      height: 400,
       alt: "Het logo van de officele KBKB",
     },
   },
